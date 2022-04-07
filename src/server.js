1f860a1f9a3e11a24ba562419a1d2d89bd62117d
Mobzaaz <mobzaaz69@gmail.com>
2022-04-07 13:49:04 +0200

    commit

--- src/server.js
+++ src/server.js
@@ -8,14 +8,10 @@ app.use(express.json());
 
 app.listen(PORT, ()=> {console.log(`Server listenning on PORT : ${PORT}`)});
 
-app.get('/getUsers', async (req, res) => {
-    const { findAll } = require('../models/agglo');
-    res.send(await findAll());
-})
-
 // Create new account
 
 app.post('/createAccount', async (req, res) => {
+
     await db('users').insert(req.body)
 
     try {
@@ -25,3 +21,24 @@ app.post('/createAccount', async (req, res) => {
     }
 
 });
+
+// Login to account
+
+app.get('/login', async (req, res) => {
+    //1 On recherche le compte à partir du body de la requête et on compare avec ce qu'il y a dans la db
+
+    const isAccount = await db('users').where({
+        email: req.body.email,
+        mot_de_passe: req.body.mot_de_passe
+      }).select('*');
+
+    if(!isAccount) {
+        res.status(404).send('Account not found !')
+    } else {
+        res.status(200).send('Welcome to your account !');
+    }
+
+    //2 Si le compte y est, on créer un json web token
+
+    //3 Si le compte n'y est pas, on affiche un message d'erreur
+})
