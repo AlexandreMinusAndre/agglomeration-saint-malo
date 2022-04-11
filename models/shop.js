68580f3c534ff4b2236b7fabae7d7e61bc7705cb
Mobzaaz <mobzaaz69@gmail.com>
2022-04-11 13:42:19 +0200

    commit

--- models/shop.js
+++
@@ -1,8 +0,0 @@
-const db = require('../models/client');
-
-const createShops = async content => {
-
-    await db('shop').insert(content);
-}
-
-module.exports = createShops;
--- src/server.js
+++ src/server.js
@@ -6,8 +6,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const DBPORT = process.env.PGPORT;
 
-const cors = require('cors');
-
 app.use(express.json());
 app.use(cors());
 
@@ -15,13 +13,9 @@ app.use(cors());
 const createAccount = require('../routes/createAccount');
 const login = require('../routes/login');
 const bonPlan = require('../routes/post');
-const createShop = require('../routes/shop');
 
 app.use('/createAccount', createAccount);
 app.use('/login', login);
 app.use('/bonPlans', bonPlan);
-app.use('/createShops', createShop);
-
 
 app.listen(PORT, ()=> {console.log(`Server listenning on PORT : ${PORT}`)});
-app.listen(DBPORT, ()=> {console.log(`Database listenning on PORT : ${DBPORT}`)});
